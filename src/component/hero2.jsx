27009971a3e5873996637589e7adeb0c62caa472
totafreelancer <totamotiur@gmail.com>
2024-04-22 04:49:55 +0600

    all add

--- src/component/hero2.jsx
+++ src/component/hero2.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../assets/css/Hero.css";
 import "../assets/images/banner-web.png";
+import logo_gif from "../assets/images/gif/logo-gif.gif"
 import BusIcon from "../assets/images/icon_svg/Bus.svg";
 import AirIcon from "../assets/images/icon_svg/air.svg";
 import ShipIcon from "../assets/images/icon_svg/Ship.svg";
@@ -13,20 +14,40 @@ const Hero = () => {
   const [input1Text, setInput1Text] = useState("Pleaseselect");
   const [input2Text, setInput2Text] = useState("Pleaseselect");
   const [outputVisible, setOutputVisible] = useState(false);
+  const [busAvailable, setBusAvailable] = useState(true); // Add state for bus availability
+  const [loading, setLoading] = useState(false); // Add state for loading
 
   const handleItemClick = (text1, text2) => {
     setInput1Text(text1);
     setInput2Text(text2);
   };
 
+  const handleSwipeClick = () => {
+    const temp = input1Text;
+    setInput1Text(input2Text);
+    setInput2Text(temp);
+  };
+
   const handleSubmit = () => {
     if (input1Text === "Pleaseselect" || input2Text === "Pleaseselect") {
       alert("Please select both 'Going From' and 'Going To' options.");
     } else {
-      setOutputVisible(true);
+      setLoading(true); // Set loading to true when submitting
+      const busData = HeroJson.filter(
+        (item) => item.from === input1Text && item.to === input2Text
+      );
+      setTimeout(() => {
+        if (busData.length === 0) {
+          setBusAvailable(false); // Set bus availability to false if no data found
+        } else {
+          setBusAvailable(true); // Set bus availability to true if data found
+          setOutputVisible(true);
+        }
+        setLoading(false); // Set loading to false after data processing
+      }, 2000); // Simulate 3 seconds delay
     }
   };
-  
+
   return (
     <div>
       <div id="hero" className="bg-cover bg-no-repeat">
@@ -64,17 +85,25 @@ const Hero = () => {
                   <div className="bdt_input relative">
                     <div>
                       <span className="bdt_input_label">Going From</span>
-                      <span className="bdt_input_value" id="input1">{input1Text}</span>
+                      <span className="bdt_input_value" id="input1">
+                        {input1Text}
+                      </span>
                     </div>
                   </div>
                   <div className="bdt_input relative">
                     <div>
                       <span className="bdt_input_label">Going To</span>
-                      <span className="bdt_input_value" id="input2">{input2Text}</span>
+                      <span className="bdt_input_value" id="input2">
+                        {input2Text}
+                      </span>
                     </div>
                   </div>
-                  <button className="btn_swipe">
-                    <img src={SwaplIcon} className="scheme-secondary" alt="icon" />
+                  <button className="btn_swipe" onClick={handleSwipeClick}>
+                    <img
+                      src={SwaplIcon}
+                      className="scheme-secondary"
+                      alt="icon"
+                    />
                   </button>
                 </div>
 
@@ -92,7 +121,6 @@ const Hero = () => {
                     </div>
                   </div>
                 </div>
-
               </div>
 
               <div className="bdt-trending-search pl-5">
@@ -100,7 +128,10 @@ const Hero = () => {
                 <ul className="list-route">
                   {HeroJson.map((item, index) => (
                     <li key={index}>
-                      <a className="cursor-pointer" onClick={() => handleItemClick(item.from, item.to)}>
+                      <a
+                        className="cursor-pointer"
+                        onClick={() => handleItemClick(item.from, item.to)}
+                      >
                         <ul className="list-route-card">
                           <li>{item.from}</li>
                           <li>{item.to}</li>
@@ -111,37 +142,55 @@ const Hero = () => {
                 </ul>
               </div>
               <div className="bdt-search-submit">
-                <button type="submit" className="btn btn-primary capitalize fw-bold" onClick={handleSubmit}>search bus</button>
+                <button
+                  type="submit"
+                  className="btn btn-primary capitalize fw-bold"
+                  onClick={handleSubmit}
+                >
+                  search bus
+                </button>
               </div>
             </div>
           </div>
         </section>
       </div>
-      {outputVisible && (
-        <section id="tota_output">
-          <table className="list_table">
-            <thead>
-              <tr>
-                <th>No:</th>
-                <th>From</th>
-                <th>To</th>
-                <th>Fare</th>
-                <th>Date</th>
-              </tr>
-            </thead>
-            <tbody>
-              {HeroJson.filter(item => item.from === input1Text && item.to === input2Text).map((item, index) => (
-                <tr key={index}>
-                  <td>{index + 1}</td>
-                  <td>{item.from}</td>
-                  <td>{item.to}</td>
-                  <td>{item.fare}</td>
-                  <td>{item.date}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </section>
+      {loading ? (
+        <div className="loading">
+          <img src={logo_gif} alt="Loading..." />
+        </div>
+      ) : (
+        <>
+          {outputVisible && busAvailable ? (
+            <section id="tota_output">
+              <table className="list_table">
+                <thead>
+                  <tr>
+                    <th>No:</th>
+                    <th>From</th>
+                    <th>To</th>
+                    <th>Fare</th>
+                    <th>Date</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {HeroJson.filter(
+                    (item) => item.from === input1Text && item.to === input2Text
+                  ).map((item, index) => (
+                    <tr key={index}>
+                      <td>{index + 1}</td>
+                      <td>{item.from}</td>
+                      <td>{item.to}</td>
+                      <td>{item.fare}</td>
+                      <td>{item.date}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </section>
+          ) : (
+            <div>{!busAvailable && <p>কোনও বাস নেই</p>}</div>
+          )}
+        </>
       )}
     </div>
   );
